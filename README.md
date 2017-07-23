99c2fb6d0333a6e650d61643b50e5b72f3540947
wahula4 <mawahula4@gmail.com>
2017-07-23 12:15:31 -0600

    updated readME

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # liri-node-app
+
+Twitter search
+node liri.js my-tweets
+
+Movie search
+in the console run -- node liri.js movie-this '<movie name here>'
+
+Spotify search
+node liri.js spotify-this-song '<song name here>'
+
+Do what it says search
+node liri.js do-what-it-says
--- liri.js
+++ liri.js
@@ -1,37 +1,41 @@
+// retrieves keys for twitter and spotify
 var keys = require("./keys.js");
 var Twitter = require('twitter');
 var Spotify = require('node-spotify-api');
 var request = require('request');
 var fs = require("fs");
 
-
 var command = process.argv[2];
 var commandTwo = process.argv[3];
+//refers to twitter keys
 var client = new Twitter(keys.twitterKeys)
+//refers to spotify keys
 var spotify = new Spotify(keys.spotifyKeys)
 
-
+	// if the user's command is my-tweets, run the tweet function
 	if (command === "my-tweets") {
-		tweet();
+		return tweet();
 	}
-
+	// if the user's command is movie-this, run the movie function
 	else if (command === "movie-this") {
-	 	movie();
+	 	return movie();
 	 }
-
+	 // if the user's command is do-what-it-says, run the doWhatItSays function
 	else if (command === "do-what-it-says") {
-		doWhatItSays();
+		return doWhatItSays();
 	}
-
+	// if the user's command is spotify-this-song, run the music function
 	else if (command === "spotify-this-song") {
-		music();
+		return music();
 	}
-
+	// function for spotify
 	function music() {
+		// if commandTwo is undefined, we will give an example of a song.
 		if (commandTwo === undefined) {
 			console.log("Pick a song next time, here's the song 'Eye of the Tiger'")
 			commandTwo = "Eye of the Tiger";
 		}
+		// there will only be one result per search
 		spotify.search({ type: 'track', query: commandTwo, limit: 1}, function(err, data) {
 			if (err) {
 			    return console.log('Error occurred: ' + err);
@@ -42,18 +46,22 @@ var spotify = new Spotify(keys.spotifyKeys)
 				console.log(JSON.stringify("URL: " + data.tracks.items[0].album.artists[0].external_urls.spotify, null, 2));
 	    });	
 	}
-
+	// tweet function
 	function tweet() {
-			var params = {screen_name: 'TonyWahula', count: 1};
+			var params = {screen_name: 'TonyWahula', count: 2};
 		client.get('statuses/user_timeline', params, function(error, tweets, response) {
 		  if (!error) {
-		    console.log(tweets[0].text);
-		    console.log(tweets[0].created_at)
-		  }
+		  	// console log every week up to count 2
+		  	for (var i = 0; i < tweets.length; i++) {
+		  		console.log(tweets[i].text);
+		    	console.log(tweets[i].created_at)
+		  	};
+		  };
 		});
-	}
-
+	};
+	 // movie function
 	 function movie() {
+	 		// if commandTwo is undefined, an example movie will be chosen
 	 		if (commandTwo === undefined) {
 	 			console.log("Pick a movie next time, here's the movie 'It'")
 				commandTwo = "It";
@@ -71,7 +79,7 @@ var spotify = new Spotify(keys.spotifyKeys)
 			console.log("URL: " + omdb.tomatoURL);
 		});
 	}
-
+	// function that read random.txt -- currently it should retrieve movie data for 'Toy Story'
 	function doWhatItSays() {
 			fs.readFile("random.txt", "utf8", function (err, data) {
 	        	if (err) {
